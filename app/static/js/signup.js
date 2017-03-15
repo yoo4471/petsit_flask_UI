25d86b7125f305e483f7acb15a180d40754384f1
yoo4471 <bell1346@gmail.com>
2017-03-15 18:57:54 +0900

    menu activate

--- app/static/js/signup.js
+++ app/static/js/signup.js
@@ -1,3 +1,8 @@
+var clicked_path = window.location.pathname
+var current_id = document.getElementById(clicked_path)
+current_id.setAttribute("aria-selected",true)
+// clicked_path.setAttribute("aria-selected",true)
+
 
 function onClicked(elem) {
   if(elem.id == "create_using_email_button") {
@@ -20,7 +25,7 @@ function onClicked_Plus(elem) {
 }
 
 function onClicked_Minus(elem) {
-  alert("Hello! I am an alert box!!");
+
   var value = parseInt(document.getElementById('input_number_of_room').value, 10);
   value = isNaN(value) ? 0 : value;
   if(value > 0) {
@@ -28,11 +33,5 @@ function onClicked_Minus(elem) {
     document.getElementById('input_number_of_room').value = value;
     document.getElementById("span_number_of_room").innerHTML="방 " + value + '개';
   }
-}
-
-function onClicked_profile(elem) {
-  console.log(elem);
-  elem.setAttribute("aria-selected",true)
-
 }
 // object.setAttribute("aria-selected",value);var value = object.getAttribute("aria-selected");
