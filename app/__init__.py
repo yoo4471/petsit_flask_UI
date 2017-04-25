5d73c88d28358f04a047b7617372dc90fc486573
soyoung <cowzero87@gmail.com>
2017-04-25 13:13:21 +0900

    add init function

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,9 @@ from app import views
 # from app import login
 from . import function
 
-function.Make_db()
+function.Make_db_member()
 function.Make_db_house()
 function.Make_db_pet()
+function.Make_db_petsitter()
+function.Make_db_image()
+function.Make_db_tran()
