162545290857f89cda5116d565c0512ad6205c7a
hyojung <hyojunghan1016@gmail.com>
2017-05-15 22:17:03 +0900

    check

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,16 @@
 from flask import Flask
+from . import function
 
 app = Flask(__name__)
 from app import views
-# from app import login
-from . import function
-from . import views
 
-# views.remove_DBfiles()
-function.Make_db_member()
-function.Make_db_house()
-function.Make_db_pet()
-function.Make_db_petsitter()
-function.Make_db_image()
-function.Make_db_tran()
-
-# views.loop_insert()
+try:
+    function.Make_db_member()
+    function.Make_db_house()
+    function.Make_db_pet()
+    function.Make_db_petsitter()
+    function.Make_db_image()
+    function.Make_db_tran()
+    views.loop_insert()
+except:
+    print("db is already existed")
--- app/function.py
+++ app/function.py
@@ -10,14 +10,14 @@ def Make_db_key(S):
     con = sqlite3.connect("petsitting.db")
     cursor = con.cursor()
     cursor.execute("CREATE TABLE IF NOT EXISTS key(key_value text NOT NULL UNIQUE)")
-    cursor.execute("INSERT INTO key(key_value) VALUES (?)", (S))
+    cursor.execute("INSERT INTO key(key_value) VALUES (?)", (S, ))
     con.commit()
     con.close()
 
 def Get_key():
     con = sqlite3.connect("petsitting.db")
     cursor = con.cursor()
-    cursor.execute("SELECT * FROM key ",)
+    cursor.execute("SELECT * FROM key ")
     data = cursor.fetchall()
     con.commit()
     con.close()
@@ -432,12 +432,12 @@ def Delete_image(Host, Asset):
 
 #총마리수로 검색하는 경우
 #result : Host, Cost_L, Cost_M, Cost_S, total, address, type
-def Search_bytotal(R, T, L, M, S, S_date, E_date):
+def Search_bytotal(T, L, M, S, S_date, E_date):
     con = sqlite3.connect("petsitting.db")
     cursor = con.cursor()
-    cursor.execute("SELECT Host, Cost_L, Cost_M, Cost_S, Total FROM petsitter WHERE Total >= ? AND Large >= ? AND Medium >= ? AND Small >=? AND Start_Date <=? AND End_Date >= ? AND Except_Date NOT BETWEEN Start_Date AND End_Date", (T, L, M, S, S_date, E_date))
+    cursor.execute("SELECT Host, Cost_L, Cost_M, Cost_S, Total, H_name FROM petsitter WHERE Total >= ? AND Large >= ? AND Midium >= ? AND Small >=? AND Start_Date <=? AND End_Date >= ? AND Except_Date NOT BETWEEN Start_Date AND End_Date", (T, L, M, S, S_date, E_date))
     data = cursor.fetchall()
-    cursor.execute("SELECT COUNT(Host) FROM petsitter WHERE Total >= ? AND Large >= ? AND Medium >= ? AND Small >=? AND Start_Date <=? AND End_Date >= ? AND Except_Date NOT BETWEEN Start_Date AND End_Date", (T, L, M, S, S_date, E_date))
+    cursor.execute("SELECT COUNT(Host) FROM petsitter WHERE Total >= ? AND Large >= ? AND Midium >= ? AND Small >=? AND Start_Date <=? AND End_Date >= ? AND Except_Date NOT BETWEEN Start_Date AND End_Date", (T, L, M, S, S_date, E_date))
     cnt = cursor.fetchone()
     if data ==[]:
         return 0
@@ -446,11 +446,7 @@ def Search_bytotal(R, T, L, M, S, S_date, E_date):
     result2 = []
     while i<cnt[0]:
         a = data[i][0]
-        if R == None:
-            cursor2.execute("SELECT Address, Type, Room FROM house WHERE Host = ?", (a, ))
-        else:
-            R2 = '%' + R + '%'
-            cursor2.execute("SELECT Address, Type, Room FROM house WHERE Address LIKE ? AND Host = ?", (R2, a, ))
+        cursor2.execute("SELECT Address, Type, Room FROM house WHERE Host = ?", (a, ))
         data2 = cursor2.fetchall()
         if data2 ==[]:
             return 0
